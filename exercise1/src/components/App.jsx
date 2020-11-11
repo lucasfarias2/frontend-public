8e7667dc4cbb066292d76977331ac0bfdfa19d4b
Lucas Farias <lukf95@gmail.com>
2020-11-11 01:51:32 -0300

    Fix

--- exercise1/src/components/App.jsx
+++ exercise1/src/components/App.jsx
@@ -1,45 +1,39 @@
 /** @jsx jsx */
+import React from 'react'
 import { css, jsx } from '@emotion/core'
 
 import { Route, Switch } from 'react-router-dom'
-import { StoreProvider, store } from './StoreProvider'
+import { store } from './StoreProvider'
 
 import About from './About'
 import Home from './Home'
 import Nav from './Nav'
 
 const App = () => {
+  const { state } = React.useContext(store)
+  const color = state.darkMode ? '#fff' : '#34374c'
+  const backgroundColor = state.darkMode ? '#34374c' : '#fff'
   return (
-    <StoreProvider>
-      <store.Consumer>
-        {({ state }) => {
-          const color = state.darkMode ? '#fff' : '#34374c'
-          const backgroundColor = state.darkMode ? '#34374c' : '#fff'
-          return (
-            <div
-              css={css`
-                background: ${backgroundColor};
-                box-sizing: border-box;
-                color: ${color};
-                min-height: 100%;
-                padding: 40px 80px;
-                transition: background 0.2s ease-out, color 0.2s ease-out;
-              `}
-            >
-              <Nav />
-              <Switch>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
-            </div>
-          )
-        }}
-      </store.Consumer>
-    </StoreProvider>
+    <div
+      css={css`
+        background: ${backgroundColor};
+        box-sizing: border-box;
+        color: ${color};
+        min-height: 100%;
+        padding: 40px 80px;
+        transition: background 0.2s ease-out, color 0.2s ease-out;
+      `}
+    >
+      <Nav />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
   )
 }
 
